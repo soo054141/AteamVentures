2b43efe40bcaba06545ec7dad4abb59a7ef75735
kimyurim <soo054141@gmail.com>
2022-04-01 18:30:46 +0900

    Add eslint 옵션 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { myTheme } from './styles/theme';
 import styled, { ThemeProvider } from 'styled-components';
 import { Container } from './Container';
--- src/Container.tsx
+++ src/Container.tsx
@@ -1,5 +1,4 @@
-import React from "react";
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const Container = styled.div`
   width: 100px;
