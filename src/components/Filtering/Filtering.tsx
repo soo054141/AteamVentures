bbf77a88c454c6714cef8c92b252bea60483a217
kimyurim <soo054141@gmail.com>
2022-04-03 01:21:59 +0900

    Update 체크박스 선택 함수

--- src/components/Filtering/Filtering.tsx
+++ src/components/Filtering/Filtering.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { FilterWrapper, Selection } from "./style";
 import SelectBtn from "../SelectBtn/SelectBtn";
 // import { FilterNameType } from "../SelectBtn/SelectBtn";
+import { useState } from "react";
 
 export enum FilterNameType {
   Method = "가공방식",
@@ -11,13 +12,16 @@ export enum FilterNameType {
 export interface SelectItemInterface {
   filterName: FilterNameType;
   selectCallback?: () => void;
+  isReset?: any;
 }
 
 export default function Filtering() {
+  const [isReset, setReset] = useState(true);
+
   return (
     <FilterWrapper>
       <Selection>
-        <SelectBtn filterName={FilterNameType.Method} />
+        <SelectBtn filterName={FilterNameType.Method} isReset={isReset} />
         <SelectBtn filterName={FilterNameType.Material} />
       </Selection>
       <div>토글영역</div>
--- src/components/SelectBtn/style.ts
+++ src/components/SelectBtn/style.ts
@@ -1,23 +1,32 @@
 import styled from "styled-components";
 
-export const SelectBox = styled.div`
-  font-size: 12px;
+export const SelectBtnContainer = styled.div`
+  display: flex;
+  background-color: blue;
 
   &:not(:first-child) {
     margin-left: 12px;
   }
 `;
+export const SelectBox = styled.div`
+  font-size: 12px;
+  background-color: white;
+`;
 
-export const Menu = styled.div`
+interface MenuInterface {
+  isClicked: boolean;
+}
+
+export const Menu = styled.div<MenuInterface>`
   display: flex;
   align-items: center;
   padding: 9px 12px;
-
-  background-color: #ffffff;
   border: 1px solid ${(props) => props.theme.colors.gray};
   border-radius: 4px;
   cursor: pointer;
 
+  background-color: ${(props) => (props.isClicked ? props.theme.colors.primary : "#ffffff")};
+  color: ${(props) => (props.isClicked ? "#ffffff" : "")};
   &:hover {
     border-color: ${(props) => props.theme.colors.secondary};
   }
@@ -34,17 +43,14 @@ export const ItemList = styled.li`
   display: flex;
   align-items: center;
   margin: 17px 12px;
-  border: 1px solid red;
 `;
 
 export const CheckBox = styled.input`
   width: 18px;
   height: 18px;
   border: 2px solid #939fa5;
-  box-sizing: border-box;
   border-radius: 2px;
   margin-right: 10px;
-  margin-left: 12px;
 `;
 
 export const ItemName = styled.div``;
